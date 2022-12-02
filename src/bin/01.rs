fe31a57cc334e3e69c02dc066a91cdd64eef4c0a
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-12-02 13:41:48 +0000

    Day 1 solution

---
+++ src/bin/01.rs
@@ -0,0 +1,49 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let vec: Vec<u32> = convert(input);
+    let max = vec.iter().cloned().max().unwrap();
+    let position = vec.iter().cloned().position(|item| item == max).unwrap();
+    Some(position as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let vec: Vec<u32> = convert(input);
+    Some(vec.iter().cloned().max().unwrap())
+}
+
+fn convert(input: &str) -> Vec<u32> {
+    let mut vec: Vec<u32> = Vec::new();
+
+    let mut total = 0;
+    for line in input.lines() {
+        if line == "" {
+            vec.push(total);
+            total = 0;
+        } else {
+            total += line.parse::<u32>().unwrap();
+        }
+    }
+    vec
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
